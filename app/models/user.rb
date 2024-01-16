2719d214a13b7eff827c6c361a05253d234d7861
Kyla (Jitera) <developers@jitera.com>
2024-01-16 11:35:52 +0000

    feat[database]: update ERD from source code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :notes, dependent: :destroy
   has_many :sessions, dependent: :destroy
+  has_many :confirmation_tokens, dependent: :destroy
 
   # validations
 
